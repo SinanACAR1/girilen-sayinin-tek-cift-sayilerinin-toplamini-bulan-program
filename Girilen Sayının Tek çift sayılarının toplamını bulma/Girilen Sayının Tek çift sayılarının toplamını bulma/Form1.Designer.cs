e3a293899adc98155d7c4c1147e1372ad4a76a18
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:09:14 +0300

    Add files via upload

---
+++ Girilen Sayının Tek çift sayılarının toplamını bulma/Girilen Sayının Tek çift sayılarının toplamını bulma/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblteksayılar = new System.Windows.Forms.Label();
+            this.lblçiftsayılar = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tek Sayılar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Çift Sayılar";
+            // 
+            // lblteksayılar
+            // 
+            this.lblteksayılar.AutoSize = true;
+            this.lblteksayılar.Location = new System.Drawing.Point(61, 95);
+            this.lblteksayılar.Name = "lblteksayılar";
+            this.lblteksayılar.Size = new System.Drawing.Size(13, 13);
+            this.lblteksayılar.TabIndex = 3;
+            this.lblteksayılar.Text = "0";
+            // 
+            // lblçiftsayılar
+            // 
+            this.lblçiftsayılar.AutoSize = true;
+            this.lblçiftsayılar.Location = new System.Drawing.Point(253, 95);
+            this.lblçiftsayılar.Name = "lblçiftsayılar";
+            this.lblçiftsayılar.Size = new System.Drawing.Size(13, 13);
+            this.lblçiftsayılar.TabIndex = 4;
+            this.lblçiftsayılar.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hangi sayının tek çift sayıları bulunsun ?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 128);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblçiftsayılar);
+            this.Controls.Add(this.lblteksayılar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tek Çift Sayı Ayıklama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblteksayılar;
+        private System.Windows.Forms.Label lblçiftsayılar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Girilen Sayının Tek çift sayılarının toplamını bulma/Girilen Sayının Tek çift sayılarının toplamını bulma/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void temizle() 
+        {
+            textBox1.Clear();
+          
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int tektoplam = 0;
+                int çifttoplam = 0;
+                int sayı;
+                sayı = Convert.ToInt32(textBox1.Text);
+                temizle();
+                if (sayı % 2 == 0)
+                {
+                    çifttoplam = çifttoplam + sayı;
+                    lblçiftsayılar.Text = çifttoplam.ToString();
+                }
+                if (sayı % 1 == 0)
+                {
+                    tektoplam = tektoplam + sayı;
+                    lblteksayılar.Text = tektoplam.ToString();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Sayı giriniz..");
+            }
+        }
+    }
+}
